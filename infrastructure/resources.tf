96a9a790f2ef69a8c524fc360fad031a5ad78cad
glaisne <glaisne@outlook.com>
2025-05-27 00:54:11 -0400

    corrected id

--- infrastructure/resources.tf
+++ infrastructure/resources.tf
@@ -56,7 +56,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
 # Connect the AKS to the ACR
 resource "azurerm_role_assignment" "acr_pull" {
-    principal_id   = azurerm_kubernetes_cluster.aks.kubelet_identity[0].client_id
+    principal_id   = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
     role_definition_name = "AcrPull"
     scope          = azurerm_container_registry.acr.id
     skip_service_principal_aad_check = true
